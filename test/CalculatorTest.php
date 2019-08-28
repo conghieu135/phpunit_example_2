ec82a072a084ec1cdd29fe74517997dd31233540
conghieu135 <conghieu135@gmail.com>
2019-08-28 10:40:35 +0700

    ccc

--- test/CalculatorTest.php
+++ test/CalculatorTest.php
@@ -9,7 +9,7 @@
 
         public function testTrueIsTrue()
         {  
-            echo "debugger";
+            // echo "debugger";
             $foo = false;
             $foo = true;
             $this->assertTrue($foo);
